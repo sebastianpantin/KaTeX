90a4bb8e715c2171bf08237833269680263b4a3e
Andrey P <prih@prihhost.com>
2016-07-22 20:08:12 +0300

    .

--- src/functions.js
+++ src/functions.js
@@ -141,7 +141,7 @@ defineFunction("\\testtag", {
     numArgs: 0,
     argTypes: ["text"]
 }, function(context, args) {
-    var body = '123';
+    var body = "123";
     // Since the corresponding buildHTML/buildMathML function expects a
     // list of elements, we normalize for different kinds of arguments
     // TODO(emily): maybe this should be done somewhere else
