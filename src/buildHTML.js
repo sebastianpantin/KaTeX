6baafa27a84dbe16a318db481488ae3fe187f7f2
Andrey P <prih@prihhost.com>
2016-07-25 16:27:00 +0300

    .

--- src/buildHTML.js
+++ src/buildHTML.js
@@ -248,7 +248,6 @@ groupTypes.testtag = function(group, options, prev) {
 
 groupTypes.input_tag = function(group, options, prev) {
     console.log(group.value.size);
-    
     return buildCommon.makeInput(3);
 };
 
