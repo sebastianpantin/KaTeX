f3f41b2275ed1fc81310934c8124acf7ced7ecea
Andrey P <prih@prihhost.com>
2016-07-25 16:43:18 +0300

    .

--- src/domTree.js
+++ src/domTree.js
@@ -327,7 +327,7 @@ function input(size, classes, children, height, depth, maxFontSize, style) {
 input.prototype.toNode = function() {
     var el = document.createElement("input");
 
-    el.type = 'text';
+    el.type = 'numeric';
 
     // Apply the class
     el.className = createClass(this.classes);
